5d2c3ef952156c860ce1fb140960c20abafbc79c
kiqisafi <121747478+kiqisafi@users.noreply.github.com>
2024-11-22 09:52:42 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('toggle-widget').addEventListener('click', function() {
+    const widget = document.getElementById('widget');
+    widget.classList.toggle('hidden'); // Mengubah kelas untuk menampilkan atau menyembunyikan widget
+});
